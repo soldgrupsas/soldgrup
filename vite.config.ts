ae3978bbe66fb2a7e122ca3e83049924454b7ca4
soldgrupsas <contacto@soldgrup.com>
2025-12-23 15:21:43 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
-  base: "/soldgrup/",   // ESTO ES CLAVE
-  server: {
-    host: "::",
-    port: 8080,
+export default defineConfig({
+  base: "/soldgrup/", // NOMBRE EXACTO DEL REPO
+  build: {
+    outDir: "docs", //  GitHub Pages
   },
-  plugins: [
-    react(),
-    mode === "development" && componentTagger()
-  ].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
